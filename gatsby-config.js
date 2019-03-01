16597e4729576b85b44cc3ed4524cd91474a8424
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2019-03-01 04:27:17 -0500

    add files

---
+++ gatsby-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  siteMetadata:{
+    title: "Lambda School",
+  },
+  plugins: [
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+}
